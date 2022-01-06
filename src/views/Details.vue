d0babc352b3eba28ec6ea1f514029346016e6f00
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-01-06 16:00:17 +0100

    added details page for single post data

---
+++ src/views/Details.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="post">
+    <h4>{{ post.title }}</h4>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+import getPost from "../composables/getPost";
+export default {
+  props: ["id"],
+  setup(props) {
+    const { load, post, error } = getPost(props.id);
+    load();
+    return { post, error };
+  },
+};
+</script>
+
+<style></style>
