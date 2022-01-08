8c195567a055c130c7e85898c1a4a543c10f1f71
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-01-08 23:18:05 +0100

    learning about ref template in setup function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="home">
     <h2>Home</h2>
-    <p>My name is {{ name }} and I'm {{ age }} years old</p>
+    <p ref="p">My name is {{ name }} and I'm {{ age }} years old</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { ref } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    console.log("setup running");
+    // refs
+    const p = ref(null);
 
+    // non-reactive values
     let name = "Gideon";
     let age = 23;
 
     const handleClick = () => {
-      console.log("I was clicked");
+      console.log(p, p.value);
+      p.value.classList.add("text");
+      p.value.textContent = "Hello Gidy";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
+<style scoped>
+.text {
+  color: red;
+}
+</style>
