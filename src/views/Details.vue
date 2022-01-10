33f0d6e3f5eda0a49b66cc8b52e32655e7b6e5e8
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-01-10 15:52:24 +0100

    imported spinner to Details page

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,15 +1,20 @@
 <template>
   <div v-if="error">{{ error }}</div>
-  <div v-if="post">
-    <h4>{{ post.title }}</h4>
-    <p>{{ post.body }}</p>
+  <div v-if="post" class="post">
+    <h3>{{ post.title }}</h3>
+    <p class="pre">{{ post.body }}</p>
+  </div>
+  <div v-else>
+    <Spinner />
   </div>
 </template>
 
 <script>
-import getPost from "../composables/getPost";
+import getPost from "@/composables/getPost";
+import Spinner from "../components/Spinner.vue";
 export default {
   props: ["id"],
+  components: { Spinner },
   setup(props) {
     const { load, post, error } = getPost(props.id);
     load();
@@ -18,4 +23,20 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.tags a {
+  margin-right: 10px;
+}
+.post {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.post p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
+}
+.pre {
+  white-space: pre-wrap;
+}
+</style>
