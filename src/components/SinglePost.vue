9e7cd417d51ce14ecdd2172d9e63ed34278b8d62
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-01-14 21:59:20 +0100

    added  singlePost Componen

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,10 +1,21 @@
 <template>
-  <h3>{{ post.title }}</h3>
+  <div>
+    <h3>{{ post.title }}</h3>
+    <p>{{ snippet }}</p>
+  </div>
 </template>
 
 <script>
+import { computed } from "@vue/reactivity";
 export default {
   props: ["post"],
+  setup(props) {
+    const snippet = computed(() => {
+      return props.post.body.slice(0, 100) + "....";
+    });
+
+    return { snippet };
+  },
 };
 </script>
 
