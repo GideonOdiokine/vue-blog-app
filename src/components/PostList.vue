4d6c1f2dffefa6845cfdc26199b30f7443b76fce
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-01-14 21:50:05 +0100

    added  singlePost Componen

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" :key="post">
-      <h3>{{ post.title }}</h3>
+      <SinglePost :post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import SinglePost from "./SinglePost.vue";
 export default {
   props: ["posts"],
+  components: { SinglePost },
+  setup(props) {
+    // console.log(props.posts);
+  },
 };
 </script>
 
---
+++ src/components/SinglePost.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>{{ post.title }}</h3>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+};
+</script>
+
+<style></style>
