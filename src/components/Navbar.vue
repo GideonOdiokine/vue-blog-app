d7259f107a0153dadd9c26e0c4314b6cf0ca5985
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-01-17 11:14:14 +0100

    final clean up

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <h1>The Gigo Blog</h1>
+    <h1>The Gojo Blog</h1>
     <nav>
       <router-link :to="{ name: 'Home' }">Home</router-link>
       <router-link :to="{ name: 'Create' }">Create Post </router-link>
